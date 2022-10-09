ca4b02c89d4d4321674f8520683924a4a1fb39f1
KBhanuDeepthi <bhanudeepthivinni915@gmail.com>
2022-10-09 20:36:34 +0530

    Added:Calculate Length of the line

---
+++ LengthOfTheLine.java
@@ -0,0 +1,14 @@
+
+public class LengthOfTheLine {
+
+	public static void main(String[] args) {
+		double x1=2;
+		double y1=3;
+		double x2=4;
+		double y2=4;
+		double length=Math.sqrt(((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1)));
+		System.out.println("Length of the line " +length);
+
+	}
+
+}
